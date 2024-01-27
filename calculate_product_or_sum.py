a52ac6215f202a20363419de3ff9f3bfae3d8ced
Maks <carcillarmax@gmail.com>
2024-01-27 08:15:40 +0800

    Calculate product or sum

--- calculate_product_or_sum.py
+++ calculate_product_or_sum.py
@@ -5,8 +5,11 @@ num1 = int(input('Enter number 1: '))
 num2 = int(input('Enter number 2: '))
 
 #product(num1, num2):
-#Multiply num1 and num2 
-#Add num1 and num2 
+def product(num1, num2):
+    #Multiply num1 and num2 
+    multiply = num1 * num2
+    #Add num1 and num2 
+    sum = num1 + num2
 
 #If the product is less than or equal to 1000:
 #Print multiply
