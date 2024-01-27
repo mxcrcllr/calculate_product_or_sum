3a10d2913c24d9a7741a963365d7db71536d4653
Maks <carcillarmax@gmail.com>
2024-01-27 08:03:51 +0800

    pseudocode

---
+++ calculate_product_or_sum.py
@@ -0,0 +1,13 @@
+
+#Input num1 from the user
+#Input num2 from the user
+
+#product(num1, num2):
+#Multiply num1 and num2 
+#Add num1 and num2 
+
+#If the product is less than or equal to 1000:
+#Print multiply
+
+#Else:
+#Print sum
